15f9ca80ffcd6ec45a66dfe34dfd81be1211cc4a
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-06-20 12:17:59 +0300

    create new application

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Ilya Schevchenko on 20.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
